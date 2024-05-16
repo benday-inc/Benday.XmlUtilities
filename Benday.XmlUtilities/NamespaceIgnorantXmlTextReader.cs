eee2b2b94f073da05265eb2468def64b530ec546
Benjamin Day <benday@benday.com>
2024-05-16 14:19:41 -0400

    trying to make it work

---
+++ Benday.XmlUtilities/NamespaceIgnorantXmlTextReader.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+namespace Benday.XmlUtilities
+{
+    public class NamespaceIgnorantXmlTextReader : XmlTextReader
+    {
+        
+        public NamespaceIgnorantXmlTextReader(Stream stream) : 
+            this(stream, 
+                new XmlParserContext(
+                    null, 
+                    new CustomXmlNamespaceManager(new CustomXmlNameTable()), 
+                    null, 
+                    XmlSpace.None))
+        {
+        }
+
+        private NamespaceIgnorantXmlTextReader(
+            Stream stream, XmlParserContext context) : 
+            base(stream, XmlNodeType.Document, context) { 
+        
+
+        }
+
+
+        public override string NamespaceURI
+        {
+            get { return string.Empty; }
+        }
+
+        public override bool Read()
+        {
+            try
+            {
+                return base.Read();
+            }
+            catch (Exception ex)
+            {
+                var nameTable = NameTable;
+
+                Console.WriteLine(ex.ToString());
+                throw;
+            }            
+        }       
+    }
+
+    public class CustomXmlNamespaceManager : XmlNamespaceManager
+    {
+        /// <summary>Initializes a new instance of the <see cref="CustomXmlNamspaceManager" /> class with the specified <see cref="System.Xml.XmlNameTable" />.</summary>
+        /// <param name="nameTable">The <see cref="System.Xml.XmlNameTable" /> to use.</param>
+        /// <exception cref="System.NullReferenceException">
+        /// <see langword="null" /> is passed to the constructor</exception>
+        public CustomXmlNamespaceManager(XmlNameTable nameTable) : base(nameTable)
+        {
+
+        }
+
+        public override string LookupPrefix(string uri)
+        {
+            try
+            {
+                return base.LookupPrefix(uri);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public override string LookupNamespace(string prefix)
+        {
+            try
+            {
+                
+
+                return base.LookupNamespace(prefix);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+
+    public class CustomXmlNameTable : NameTable
+    {
+        public override string Get(char[] key, int start, int len)
+        {
+            return base.Get(key, start, len);
+        }
+
+        public override string Get(string value)
+        {
+            return base.Get(value);
+        }
+    }
+}
---
+++ Benday.XmlUtilities/XmlFormatter.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Benday.XmlUtilities
+{
+
+    public class XmlFormatter
+    {
+        public string? Format(string xml)
+        {
+            // var doc = XDocument.Parse(xml);
+
+            // var xmlSurrounded = "<root>" + xml + "</root>";
+
+            // var doc = LoadXmlIgnoringNamespaces(xmlSurrounded);
+
+            var returnValue = BeautifyXmlUsingReader(xml, true);
+
+            return returnValue.ToString();
+
+            /*
+            var reader = new NamespaceIgnorantXmlTextReader(new StringReader(xml));
+
+            var outerXml = reader.ReadOuterXml();
+
+            var doc = XDocument.Load(reader);
+
+            var element = doc.Root;
+
+            return Format(element);
+            */
+        }
+
+        private string Format(XElement element)
+        {
+            return element.ToString();
+        }
+
+        private XDocument LoadXmlIgnoringNamespaces(string xml)
+        {
+            using (StringReader stringReader = new StringReader(xml))
+            {
+                XmlReaderSettings settings = new XmlReaderSettings
+                {
+                    NameTable = new NameTable()
+                };
+                XmlNamespaceManager namespaceManager = new XmlNamespaceManager(settings.NameTable);
+                XmlParserContext context = new XmlParserContext(null, namespaceManager, null, XmlSpace.None);
+                XmlReader reader = XmlReader.Create(stringReader, settings, context);
+
+                return XDocument.Load(reader);
+            }
+        }
+
+        private string BeautifyXmlUsingReader(string xmlToBeautify, bool indentAttributes)
+        {
+            StringBuilder beautifiedXml = new StringBuilder();
+
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.Indent = true;
+            settings.IndentChars = "     ";
+            settings.NewLineOnAttributes = indentAttributes;
+
+            var writer = XmlTextWriter.Create(new StringWriter(beautifiedXml), settings);
+
+            var byteArray = Encoding.UTF8.GetBytes(xmlToBeautify);
+
+            var stream = new MemoryStream(byteArray);
+
+            var reader = new NamespaceIgnorantXmlTextReader(stream);
+
+            // var reader = new XmlTextReader(new StringReader(xmlToBeautify)); // { Namespaces = false });            
+
+            while (reader.Read())
+            {
+                switch (reader.NodeType)
+                {
+                    case XmlNodeType.Element:
+
+                        writer.WriteStartElement(reader.Name);
+                        writer.WriteAttributes(reader, false);
+                        if (reader.IsEmptyElement == true)
+                        {
+                            writer.WriteEndElement();
+                        }
+                        break;
+                    case XmlNodeType.CDATA:
+                        writer.WriteCData(reader.Value);
+                        break;
+                    case XmlNodeType.Comment:
+                        writer.WriteComment(reader.Value);
+                        break;
+                    case XmlNodeType.SignificantWhitespace:
+                        writer.WriteWhitespace(reader.Value);
+                        break;
+                    case XmlNodeType.Text:
+                        writer.WriteString(reader.Value);
+                        break;
+                    case XmlNodeType.EndElement:
+                        writer.WriteEndElement();
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+
+            writer.Flush();
+
+            var returnValue = beautifiedXml.ToString();
+
+            return returnValue;
+        }
+    }
+}
